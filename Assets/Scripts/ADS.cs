0db4ce0df131bb89e8e1d11b57d12207b419cc5b
MazankaQQW <136856664+MazankaQQW@users.noreply.github.com>
2023-07-20 02:32:00 +0300

    Add Advertising

---
+++ Assets/Scripts/ADS.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+ 
+public class ADS: MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOSGameId;
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+    string _adUnitId = "Interstitial_Android";
+    public static ADS instance;
+ 
+    void Awake()
+    {
+        InitializeAds();
+        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+    }
+ 
+    public void InitializeAds()
+    {
+    #if UNITY_IOS
+            _gameId = _iOSGameId;
+    #elif UNITY_ANDROID
+            _gameId = _androidGameId;
+    #elif UNITY_EDITOR
+            _gameId = _androidGameId; //Only for testing the functionality in the Editor
+    #endif
+        if (!Advertisement.isInitialized && Advertisement.isSupported)
+        {
+            Advertisement.Initialize(_gameId, _testMode, this);
+        }
+    }
+    
+    public void ShowAd()
+    {
+        // Note that if the ad content wasn't previously loaded, this method will fail
+        Debug.Log("Showing Ad: " + _adUnitId);
+        Advertisement.Show(_adUnitId, this);
+    }
+
+ 
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+ 
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        
+    }
+}
--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -78,11 +78,16 @@ public class Ball : MonoBehaviour
             hasCollided = true;
             StartCoroutine(RestartScene());
         }
+        
     }
 
     private System.Collections.IEnumerator RestartScene()
     {
         yield return new WaitForSeconds(restartDelay);
+        if (Score.instance.score > 0)
+        {
+            ADS.instance.ShowAd();
+        }
         LooseGame();
     }
     
